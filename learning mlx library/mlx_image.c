58ff30d20b78bd31be6cc5e3e372442ef6ac745c
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-08 19:09:16 +0100

    added mlx_image

---
+++ learning mlx library/mlx_image.c
@@ -0,0 +1,33 @@
+#include <mlx.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int main()
+{
+	void	*mlx;
+	void	*win;
+	t_data	img;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1920, 1080, "new window!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x10FFFFFF);
+	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
