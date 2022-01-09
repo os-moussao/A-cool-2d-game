06a6b778dcc4e09b0f0353698c3ba15f22822ee1
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-09 23:38:25 +0100

    started so_long

---
+++ the game/include/so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 22:11:39 by omoussao          #+#    #+#             */
+/*   Updated: 2022/01/09 23:37:07 by omoussao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <mlx.h>
+# include <fcntl.h>
+
+# define BUFFER_SIZE 100000
+
+#endif
