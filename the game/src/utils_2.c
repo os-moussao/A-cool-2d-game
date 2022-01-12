d579c5eaa29a033c393c995170484d5aa16269fc
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-12 19:11:54 +0100

    added get_player_pos function

--- the game/src/utils_2.c
+++ the game/src/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 20:03:22 by omoussao          #+#    #+#             */
-/*   Updated: 2022/01/10 20:13:05 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/01/12 18:26:55 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,29 @@ void	display_map(t_map map)
 			printf("%c", map.map[i][j]);
 		printf("\n");
 	}
+}
+
+t_pos	get_player_pos(t_map map)
+{
+	int		i;
+	int		j;
+	t_pos	pos;
+
+	pos.i = -1;
+	pos.j = -1;
+	i = -1;
+	while (++i < map.height)
+	{
+		j = -1;
+		while (++j < map.width)
+		{
+			if (map.map[i][j] == 'P')
+			{
+				pos.i = i;
+				pos.j = j;
+				return (pos);
+			}
+		}
+	}
+	return (pos);
 }
