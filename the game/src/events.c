33f7d4ce3ab0109ae5f2d9f8747fa8831149e41f
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-12 19:15:11 +0100

    complete game logic

--- the game/src/events.c
+++ the game/src/events.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 22:10:34 by omoussao          #+#    #+#             */
-/*   Updated: 2022/01/11 22:54:57 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/01/12 19:14:37 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,40 @@ int	close_prog(t_params *params)
 	return (0);
 }
 
+void	mv_player(int key, t_params *params)
+{
+	int			i;
+	int			j;
+	t_pos		old;
+	bool		change;
+	static int	moves;
+
+	change = 0;
+	old = params->map.player_pos;
+	i = old.i + (key == KEY_S) - (key == KEY_W);
+	j = old.j + (key == KEY_D) - (key == KEY_A);
+	if (params->map.map[i][j] == '0' || params->map.map[i][j] == 'C')
+		change = 1;
+	if (params->map.map[i][j] == 'C')
+		(params->map.coll)--;
+	if (params->map.map[i][j] == 'E' && params->map.coll == 0)
+		close_prog(params);
+	if (change)
+	{
+		params->map.map[old.i][old.j] = '0';
+		params->map.map[i][j] = 'P';
+		params->map.player_pos.i = i;
+		params->map.player_pos.j = j;
+		printf("Moves: %d\n", ++moves);
+		render_map(params);
+	}
+}
+
 int	key_press(int key, t_params *params)
 {
-	printf("%d\n", key);
 	if (key == KEY_ESC)
 		close_prog(params);
+	if (key == KEY_W || key == KEY_A || key == KEY_D || key == KEY_S)
+		mv_player(key, params);
 	return (0);
 }
